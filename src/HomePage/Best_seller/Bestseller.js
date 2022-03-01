2c2feeb604721ce68cf5dda0e5a7392d94a93aed
PratikshaSurwade <89350917+PratikshaSurwade@users.noreply.github.com>
2022-03-02 00:16:49 +0530

    Initialize with Navbar done

---
+++ src/HomePage/Best_seller/Bestseller.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Bestseller() {
+    return(
+        <>
+        Bestsell
+
+        </>
+    )
+}
+export default Bestseller;
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import "./navbar.css";
+import mainImg from "./corousel_3.png"
+
+import { NavLink } from "react-router-dom";
+
+
+class Heading extends React.Component{
+
+    render(){
+        return(
+            <>
+                
+                <div className="navBar">
+                    <hr className="hrTopNavbar"></hr>
+                    <h1 className="headstyle">
+                        iSHOP
+                    </h1>
+                    <div className="Navbarstyle" id="Navbarstyle2">
+                        <div className="Linkitems">HOME</div>
+                        <div className="Linkitems">STORE</div>
+                        <div className="Linkitems">IPHONE</div>
+                        <div className="Linkitems">IPAD</div>
+                        <div className="Linkitems">MACBOOK</div>
+                        <div className="Linkitems">ACCESORIES</div>
+
+                    </div>
+                    
+                    <hr style={{marginLeft:"10%",marginRight:"10%"}}></hr>
+                </div>
+                <img  className="mainImg" src={mainImg} />
+                
+            </>
+        )
+    }
+}
+
+export default Heading;
