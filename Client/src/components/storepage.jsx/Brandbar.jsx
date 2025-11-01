3ff78d26adb67c50b9e980e57916b73fcf77df17
PratikshaSurwade <89350917+PratikshaSurwade@users.noreply.github.com>
2025-11-01 23:24:27 +0530

    New components added

---
+++ Client/src/components/storepage.jsx/Brandbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Brandbar = () => {
+  return (
+    <div>Brandbar</div>
+  )
+}
+
+export default Brandbar
---
+++ Client/src/components/storepage.jsx/Colorbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Colorbar = () => {
+  return (
+    <div>Colorbar</div>
+  )
+}
+
+export default Colorbar
