6ad76474e9d8c28dc8aeeb5335a0a42dfb45f50b
PratikshaSurwade <89350917+PratikshaSurwade@users.noreply.github.com>
2025-12-22 22:48:52 +0530

    Update Error solved

--- Client/src/components/Hero/Card.jsx
+++ Client/src/components/Hero/Card.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div className='h-50 w-35 border-5 border-gray-300 flex flex-col flex-nowrap items-center'>
-        <img src='./images/storepage/2.png' alt='Product Image' />
+    <div className='h-50 w-35 border-5 border-gray-300 flex flex-col flex-nowrap items-center justify-around'>
+        <img src='./images/storepage/11.png' className='w-full h-2/4' alt='Product Image' />
         <h3>Title2</h3>
-        <div className='flex'><p className='itallic'>2000$</p><p>3000$</p></div>
+        <div className='w-full flex justify-around'><p className='itallic'>2000$</p><p className='del'>3000$</p></div>
         <div>
 
         </div>
